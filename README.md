7680d6a6dc6bc87d4ce18f5afd058daa35948a66
Yunze Xu <xyzinfernity@163.com>
2022-01-26 21:03:34 +0800

    Add ProduceAndConsume

--- README.md
+++ README.md
@@ -2,29 +2,23 @@
 
 https://github.com/apache/pulsar/issues/13965
 
+Before following steps, run `mvn clean compile` to build the project. And run a Pulsar standalone with `subscriptionKeySharedUseConsistentHashing=true`.
+
 ## How to reproduce
 
-Before following steps, run `mvn clean compile` to build the project.
-
-1. Run a Pulsar standalone with `subscriptionKeySharedUseConsistentHashing=true`.
-2. Run
-3.
-
-1. Run a Pulsar standalone with `subscriptionKeySharedUseConsistentHashing=true`.
-
-2. Run 3 producers to send 3000 messages to 3 topics with the same topic prefix
+1. Run 3 producers to send 3000 messages to 3 topics with the same topic prefix
 
    ```bash
    mvn exec:java -Dexec.mainClass='Produce'
    ```
 
-3. Run 3 consumers to subscribe with regex pattern and `Key_Shared` subscription. 
+2. Run 3 consumers to subscribe with regex pattern and `Key_Shared` subscription. 
 
    ```bash
    mvn exec:java -Dexec.mainClass='Consume'
    ```
 
-Here are the output of 5 results in my local env:
+Since no acknowledgements are done in step 2, we can repeat step 2 and here are the output of 5 results in my local env:
 
 ```
 1337
@@ -64,3 +58,9 @@ Here are the output of 5 results in my local env:
 The result of first time is wrong.
 
 You can modify the subscription name, subscription type and receive timeout in `Consume.java` to see more results.
+
+If you only want to run consumer once, you can just combine the steps in previous section:
+
+```bash
+mvn exec:java -Dexec.mainClass='ProduceAndConsume'
+```
---
+++ src/main/java/ProduceAndConsume.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import org.apache.pulsar.client.admin.PulsarAdmin;
+import org.apache.pulsar.client.admin.PulsarAdminException;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+
+public class ProduceAndConsume {
+    public static void main(String[] args) throws PulsarClientException {
+        final String topicNamePrefix = "KeySharedConsumerTest-multi-topics";
+        final PulsarAdmin admin = PulsarAdmin.builder().serviceHttpUrl("http://localhost:8080").build();
+        for (int i = 0; i < 3; i++) {
+            try {
+                admin.topics().delete(topicNamePrefix + i);
+            } catch (PulsarAdminException e) {
+                System.out.println("Failed to delete " + topicNamePrefix + i + ": " + e.getMessage());
+            }
+        }
+        admin.close();
+
+        final PulsarClient client = PulsarClient.builder().serviceUrl("pulsar://localhost:6650").build();
+        final Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            final Producer<String> producer = client.newProducer(Schema.STRING)
+                    .topic(topicNamePrefix + i)
+                    .enableBatching(false)
+                    .create();
+            for (int j = 0; j < 1000; j++) {
+                final int key = random.nextInt(100);
+                producer.newMessage(Schema.STRING).value("msg-" + j).key(key + "").sendAsync();
+            }
+            producer.flush();
+        }
+
+        final List<Consumer<String>> consumers = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            consumers.add(client.newConsumer(Schema.STRING)
+                    .topicsPattern(".*KeySharedConsumerTest-multi-topics.*")
+                    .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+                    .subscriptionType(SubscriptionType.Key_Shared)
+                    .subscriptionName("sub")
+                    .subscribe());
+        }
+        final List<Integer> numReceivedList = new ArrayList<>();
+        int numTotal = 0;
+        for (final Consumer<String> consumer : consumers) {
+            int n = 0;
+            while (true) {
+                final Message<String> msg = consumer.receive(1, TimeUnit.SECONDS);
+                if (msg == null) {
+                    break;
+                }
+                n++;
+                //System.out.println(i + " receives " + msg.getValue()
+                //        + " from " + msg.getMessageId() + " key is " + msg.getKey());
+            }
+            numReceivedList.add(n);
+            numTotal += n;
+        }
+        numReceivedList.forEach(System.out::println);
+        System.out.println(numTotal);
+        client.close();
+    }
+}
